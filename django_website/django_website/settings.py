618dcbd138cb63a1b61a9eec98760411b22efa0e
Mizuki <mizuki07190112@gmail.com>
2022-04-29 17:59:51 +0900

    second commit

--- django_website/django_website/settings.py
+++ django_website/django_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--_8+t_40&r04khlzp)xh!5g77-xfb317fhl0()#pdmka+u3%j4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'website'
 ]
 
 MIDDLEWARE = [
